0db261cfa7e1e2cf4c065e937ad9114f1b5e1568
sadik126 <sadikhimel04@gmail.com>
2021-03-16 22:20:05 +0600

    lab task 2

--- Lab task 2/first page .php
+++ Lab task 2/first page .php
@@ -1,128 +1,223 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>PHP Form Validation</title>
 </head>
 <body>
-
 <?php
-// define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $bloodgrupErr = $degree = "";
-$name = $email = $gender = $dateofbirth = $degree = $bloodgroup ="";
+ $ername=  $eremail= $erdateofbirth = $ergender= $erdegree= $erblood_group= "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-  }
+ $name=  $email= $dateofbirth = $gender= $degree= $blood_group= "";
+
+      
+
+
+    if($_SERVER["REQUEST_METHOD"]=="POST")
+    {
+        if (!empty($_POST["name"])) 
+       { 
+    
+        if(strlen($_POST['name'])>=2 && (!preg_match("/^[a-zA-Z\s]+$/", $name)))
+                {
+                   $name = $_POST['name'];
+                   // echo "Name: ".$name."<br/;>";
+                  } 
+              else
+                  {
+                   $ername= " At Least Two Letters & must be alphabets "; 
+                  }
+                    
+         }
+      
+        
   
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-  }
+        
+        if (empty($_POST["email"]))
+        {
+            $eremail = "Email is required";
+        } 
+       else 
+       {
+           $email = validate($_POST["email"]);
+       }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+        {
+            $eremail = "Invalid Email format";
+        }
+        
+      }
+    
+        
+
+
+        if (empty($_POST["dateofbirth"])) 
+        {
+          $erdateofbirth="";   
+        }
+        else
+        {
+            
+        $dateofbirth= validate($_POST["dateofbirth"]);
+        }
+
+
+        
+        if (empty($_POST["gender"])) 
+        {
+          $ergender="";   
+        }
+        else
+        {
+            $gender = validate ($_POST["gender"]);
+        }
+         
+    
+
+
+        // if (empty($_POST["degree"])) 
+        // {
+        //   $erdegree="";   
+        // }
+        // else
+        // {
+            
+        // $degree = validate($_POST["degree"]);
+        // }
+
+        
+
+        if (empty($_POST["blood_group"])) 
+        {
+          $erblood_group=" "; 
+        }
+        
+      else
+        {
+            
+        $blood_group = validate($_POST["blood_group"]);
+        }
+        
+        
+        
+
+    
+         
+
+    
+
+        function validate($data)
+        { 
+              $data= trim($data);
+              $data = stripcslashes($data);
+              $data = htmlspecialchars($data);
+              return $data;
+        }
 
 
  
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
 ?>
 
+  
+  <h1 style="text-align:center">PHP Form Validation</h1>
+  
+  <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" >
+    <fieldset>
+    <legend align="center" style="font-size: 2.0em">Fill Up the Form</legend>
 
+   <table cellpadding="2" width="50%"  align="center"cellspacing="2">
+    <tr>
+      <td>NAME</td>
+      <td><input type="text" name="name" size="30" required="1"><?php echo $ername; ?></td>
+    </tr>
 
+    <tr>
+      <td>EMAIL</td>
+      <td><input type="text" name="email" size="30" required="1"><?php echo $eremail; ?></td>
+    </tr>
+
+    <tr>
+      <td>DATE OF BIRTH</td>
+      <td><input type="Date" name="dateofbirth" size="30" min="1953-01-01" max="1998-01-01" required="1"><?php echo $erdateofbirth; ?></td>
+    </tr>
+    <tr>
+      <td>GENDER</td>
+      <td><input type="radio" name="gender" value="Male" size="30" >Male
+      <input type="radio" name="gender" value="Female" size="30" >Female
+      <input type="radio" name="gender" value="Other" size="30" >Other
+       <?php echo $ergender;?>
+    </td>
+    </tr>
 
     
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-    <p>Regestration form</p>
-    <p><b>NAME :</b></p>
-    <input type = "text" name="name "><br>
-    &#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;<br>
+    <tr>
+       <td>DEGREE</td>
+
+       
+           <td >
+            <input type="checkbox" name="degree[]" value="SSC" >SSC
+               <input type="checkbox" name="degree[]" value="HSC"  >HSC
+               <input type="checkbox" name="degree[]" value="BSc" >BSc
+               <input type="checkbox" name="degree[]" value="MSc" >MSc
+               <?php echo $erdegree; ?>
+            </td>
+        </tr>
+
+        <tr>
+         <td>BLOOD GROUP</td>
+        <td><select name="blood_group">
+        <option value="1" selected>Select a blood group</option>
+        <option value="A+">A+</option>
+        <option value="A-">A-</option>
+        <option value="B+">B+</option>
+        <option value="B-">B-</option>
+        <option value="O+">O+</option>
+        <option value="O-">O-</option>
+        <option value="AB+">AB+</option>
+        <option value="AB-">AB-</option>
+        </select></td>
+        <?php echo $erblood_group; ?>
+        </tr>
+        <tr></tr>
+        <tr>
+          <td></td>
+          <td><input type="Submit" name="submit" value="Submit"</td>
+        </tr>
+  </table>
 
 
+        
     
-
-
-    <p><b>EMAIL :</b></p>
-    <input type = "text" name="email "><br>
-    &#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;<br>
-    
-
-
-       <p><b>DATE OF BIRTH :</b></p>
-    <input type = "date" name="date of birth "><br>
-    &#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;<br>
-    
-
-      <p><b>GENDER :</b></p>
-    <input type="radio"   value="male">
-  <label for="male">male</label>
-  <input type="radio"   value="female">
-  <label for="female">female</label>
-  <input type="radio"   value="other">
-  <label for="other">other</label><br>
-    &#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;<br>
- 
-
-
-      <p><b>DEGREE :</b></p>
-    <input type="radio"   value="ssc">
-  <label for="ssc">SSC</label>
-  <input type="radio"   value="hsc">
-  <label for="hsc">HSC</label>
-  <input type="radio"   value="bsc">
-  <label for="bsc">Bsc</label>
-  <input type="radio"   value="msc">
-  <label for="msc">Msc</label><br>
-    &#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;<br>
-    
-
-
-    <p><b>BLOOD GROUP:</b></p>
-    <select>
-            <option></option>
-            <option>A+</option>
-            <option>A-</option>
-            <option>B+</option>
-            <option>B-</option>
-            <option>O+</option>
-            <option>O-</option>
-        </select><br>
-        &#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;&#8212;<br>
-
-        <input type="submit" name="submit" value="Submit"> 
-    
-    </form>
+</fieldset>
+</form>
 
 
 
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $dateofbirth;
-echo "<br>";
-echo $gender;
-echo "<br>";
-echo $degree;
-echo "<br>";
-echo $bloodgroup;
+<?php  
 
 
-    ?>
+ echo "<h2> Input:</h2>";
+ echo "NAME : ".$name."<br>";
+ echo "EMAIL : ".$email."<br>";
+ echo "DATE OF BIRTH : ".$dateofbirth."<br>";
+if(isset($_POST["submit"]))
+{
+  if(!empty($_POST["degree"]))
+  {
+    foreach($_POST["degree"] as $degree)
+    {
+      echo "degree : ".$degree."<br>";
+    }
+  }
+  else
+  {
+    echo'please select this';
+  }
+}
+echo "gender: ".$gender."<br>";
+ echo "BLOOD GROUP : ".$blood_group."<br>";
 
+
+?>
 </body>
 </html>
